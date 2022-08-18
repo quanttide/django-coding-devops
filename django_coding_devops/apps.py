d0ca8b14126cb14a3a294ac1fa492cda87581c8e
Guo Zhang <zhangguo@quanttide.com>
2022-08-18 20:53:16 +0800

    init: create project

---
+++ django_coding_devops/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DjangoCodingDevOpsConfig(AppConfig):
+    name = 'django_coding_devops'
--- tests/test_apps.py
+++ tests/test_apps.py
@@ -2,9 +2,9 @@ from django.test import SimpleTestCase, override_settings
 from django.apps import apps
 
 
-@override_settings(INSTALLED_APPS=['django_package.apps.DjangoPackageConfig'])
+@override_settings(INSTALLED_APPS=['django_coding_devops.apps.DjangoCodingDevOpsConfig'])
 class AppConfigTestCase(SimpleTestCase):
     def test_install_app(self):
-        self.assertTrue(apps.is_installed('django_package'))
-        app_config = apps.get_app_config('django_package')
-        self.assertEqual('django_package', app_config.name)
+        self.assertTrue(apps.is_installed('django_coding_devops'))
+        app_config = apps.get_app_config('django_coding_devops')
+        self.assertEqual('django_coding_devops', app_config.name)
